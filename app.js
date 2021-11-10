7284d011aca412c4b7f9ffab0655fefa4bdc6136
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-11-10 14:16:56 +0530

    update : test build

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send("Welcome to AWS bro !!!!")
+    res.send("Welcome to AWS Bro !! Its working!!")
 })
 const port = process.env.port || 3000
 app.listen(port,()=>{
